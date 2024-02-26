7e3c4463878d568c0c03bca48a9b09cbed137176
ArshhGill <arshh.gill@outlook.com>
2024-02-26 11:01:19 +0530

    inorder

--- internals/bst/bst.cpp
+++ internals/bst/bst.cpp
@@ -7,9 +7,49 @@ struct Node {
     Node *right;
 };
 
-class bst {
-private:
-     
-};
+Node *createNode(const int val) {
+    Node *newNode = new Node();
+    newNode->val = val;
+    newNode->left = newNode->right = nullptr;
 
-int main() { return 0; }
+    return newNode;
+}
+
+Node *insertNode(Node *root, const int val) {
+    if (!root) {
+        root = createNode(val);
+        return root;
+    }
+    if (val <= root->val) {
+        root->left = insertNode(root->left, val);
+    } else if (val > root->val) {
+        root->right = insertNode(root->right, val);
+    }
+
+    return root;
+}
+
+void inorder(Node *root) {
+    if (!root) {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->val << endl;
+    inorder(root->right);
+}
+
+// get val, insert, delete, search, findMin, findMax, findHeight,
+// levelordertraversal, preOrder, inOrder, postOrder
+
+int main() {
+    auto root = insertNode(nullptr, 5);
+    root = insertNode(root, 7);
+    root = insertNode(root, 8);
+    root = insertNode(root, 0);
+    root = insertNode(root, 69);
+
+    inorder(root);
+
+    return 0;
+}
