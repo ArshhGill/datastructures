7abbcaed3c1fd81a9e934afd3df1a1802eb7694c
ArshhGill <arshh.gill@outlook.com>
2024-02-27 09:43:40 +0530

    destroyer

--- internals/bst/bst.cpp
+++ internals/bst/bst.cpp
@@ -29,7 +29,27 @@ Node *insertNode(Node *root, const int val) {
     return root;
 }
 
-void inorder(Node *root) {
+void postorder(Node *root) { // orders you post
+    if (!root) {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << endl;
+}
+
+void preorder(Node *root) { // orders you pre
+    if (!root) {
+        return;
+    }
+
+    cout << root->val << endl;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root) { // orders you in
     if (!root) {
         return;
     }
@@ -39,7 +59,7 @@ void inorder(Node *root) {
     inorder(root->right);
 }
 
-bool searcher(Node *root, int val) {
+bool searcher(Node *root, int val) { // searches you
     if (!root) {
         return false;
     }
@@ -53,6 +73,48 @@ bool searcher(Node *root, int val) {
     }
 }
 
+Node *findMax(Node *root) {
+    if (!root) {
+        return root;
+    }
+    if (root->right) {
+        return findMax(root->right);
+    } else {
+        return root;
+    }
+}
+
+Node *destroyer(Node *root, int val) { // destroys you
+
+    if (!root) {
+        return nullptr;
+    }
+    if (val < root->val) {
+        root->left = destroyer(root->left, val);
+    } else if (val > root->val) {
+        root->right = destroyer(root->right, val);
+    } else {
+        if (!root->left && !root->right) {
+            delete root;
+            root = nullptr;
+        } else if (!root->right && root->left) {
+            auto uwu = root;
+            root = root->left;
+            delete uwu;
+        } else if (root->right && !root->left) {
+            auto uwu = root;
+            root = root->right;
+            delete uwu;
+        } else {
+            auto uwu = findMax(root->left);
+            root->val = uwu->val;
+            root->left = destroyer(uwu, uwu->val);
+            uwu = nullptr;
+        }
+    }
+    return root;
+}
+
 // get val, insert, delete, search, findMin, findMax, findHeight,
 // levelordertraversal, preOrder, inOrder, postOrder
 
@@ -62,7 +124,10 @@ int main() {
     root = insertNode(root, 8);
     root = insertNode(root, 0);
     root = insertNode(root, 69);
+    inorder(root);
+    root = destroyer(root, 5);
 
-    cout << searcher(root, 69) << endl; // deep shit
+    inorder(root);
+
     return 0;
 }
