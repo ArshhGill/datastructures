812efb639f1dc02b43cc48e9fdc3f1aa25327b00
ArshhGill <arshh.gill@outlook.com>
2024-03-05 09:52:27 +0530

    BST: Implement insert and inorder

--- internals/bst/bst.c
+++ internals/bst/bst.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+typedef struct Node Node;
 
-    printf("uwu");
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+};
+
+Node *createNode(int val) {
+    Node *uwu = malloc(sizeof(Node));
+    uwu->val = val;
+    uwu->left = uwu->right = NULL;
+    return uwu;
+}
+
+void inorder(Node *root) {
+    if (!root) {
+        return;
+    }
+
+    inorder(root->left);
+    printf("%d\n", root->val);
+    inorder(root->right);
+}
+
+Node *insert(Node *root, int val) {
+    if (!root) {
+        return createNode(val);
+    } else if (val > root->val) {
+        root->right = insert(root->right, val);
+    } else if (val < root->val) {
+        root->left = insert(root->left, val);
+    }
+
+    return root;
+}
+
+int main() {
+    Node *root = insert(NULL, 1);
+    insert(root, 2);
+    insert(root, 6);
+    insert(root, 16);
+    insert(root, 32);
+
+    printf("uwu\n");
+
+    inorder(root);
 
     return 0;
 }
